e0fd24bed18c4ab60e5cb1e1c3b417cfe93da3bf
gerardroyo <gerard.royo@mataro.epiaedu.cat>
2019-10-18 19:24:42 +0200

    APP FINISHED

--- app/src/main/java/com/project/calculadoradivisas/MainActivity.java
+++ app/src/main/java/com/project/calculadoradivisas/MainActivity.java
@@ -1,19 +1,26 @@
+
+// Developed by Gerard Royo i Plà
+// Twitter: @RoshsKipo
+
 package com.project.calculadoradivisas;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private Button buttonComa;
-    private Button buttonIgual;
-    private Button buttonSup;
-    private Button buttonCE;
+import java.text.DecimalFormat;
+
+
+public class MainActivity extends AppCompatActivity {
 
     private Button buttonLliures;
     private Button buttonDollar;
@@ -21,25 +28,19 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonYuan;
 
     private TextView textViewNumEuro;
+    private TextView textViewNumDivisa;
 
     private Button button;
+    private boolean MonedaSeleccionada = false;
+    private double numConvercio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        buttonIgual = findViewById(R.id.buttonIgual);
-        buttonSup = findViewById(R.id.buttonSup);
-        buttonCE = findViewById(R.id.buttonCE);
-
-        buttonLliures = findViewById(R.id.buttonLliures);
-        buttonDollar = findViewById(R.id.buttonDollar);
-        buttonYen = findViewById(R.id.buttonYen);
-        buttonYuan = findViewById(R.id.buttonYuan);
-
         textViewNumEuro = findViewById(R.id.textViewNumEuro);
+        textViewNumDivisa = findViewById(R.id.textViewNumDivisa);
 
         button =  findViewById(R.id.button0);
         button.setOnClickListener(new View.OnClickListener(){
@@ -128,30 +129,184 @@ public class MainActivity extends AppCompatActivity {
                 IntroduirValorEuro(",");
             }
         });
+
+        button = findViewById(R.id.buttonIgual);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mostrarConvercio();
+            }
+        });
+
+        button = findViewById(R.id.buttonSup);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                BorrarUn();
+            }
+        });
+
+        button = findViewById(R.id.buttonCE);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                BorrarTot();
+            }
+        });
+
+        buttonLliures =  findViewById(R.id.buttonLliures);
+        buttonLliures.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (MonedaSeleccionada == false) {
+                    conversioMoneda("LLIURE");
+                }
+            }
+        });
+
+        buttonDollar =  findViewById(R.id.buttonDollar);
+        buttonDollar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (MonedaSeleccionada == false) {
+                    conversioMoneda("DOLLAR");
+                }
+            }
+        });
+
+        buttonYen =  findViewById(R.id.buttonYen);
+        buttonYen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (MonedaSeleccionada == false) {
+                    conversioMoneda("YEN");
+                }
+            }
+        });
+
+        buttonYuan =  findViewById(R.id.buttonYuan);
+        buttonYuan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (MonedaSeleccionada == false) {
+                    conversioMoneda("YUAN");
+                }
+            }
+        });
     }
 
     private void IntroduirValorEuro(String valor) {
 
         String numTextEuro = textViewNumEuro.getText().toString();
 
-        if (numTextEuro.equals("0") && valor != (",")) {
-            numTextEuro = "";
-            textViewNumEuro.setText(numTextEuro + valor);
-        } else {
-
-            if (numTextEuro.indexOf(",") >= 0) {
-                if(numTextEuro.substring(numTextEuro.indexOf(",")).length() <= 2) {
-                    if (numTextEuro.equals("0")) {
-                        numTextEuro = "";
+        if (valor == ",") {
+            if (numTextEuro.indexOf(',') <= 0) {
+                textViewNumEuro.setText(numTextEuro + valor);
+            }
+        } else if (numTextEuro.indexOf(',') >= 0) { // Comprova si existeix una coma
+            if (numTextEuro.substring(numTextEuro.indexOf(",")).length() <= 2) { // Si existeix, conta quants caràcters hi han a partir de la coma. Si hi han 2, deixa d'introduir números.
+                if (numTextEuro.equals("0")) {
+                    numTextEuro = "";
                 }
+
+                textViewNumEuro.setText(numTextEuro + valor);
+            }
+        }
+        else {
+            if (numTextEuro.equals("0")) {
+                numTextEuro = "";
             }
 
             textViewNumEuro.setText(numTextEuro + valor);
         }
-
-
-
-
     }
 
+    private void BorrarTot() {
+        textViewNumEuro.setText("0");
+        textViewNumDivisa.setText("0");
+    }
+
+    private void BorrarUn() {
+
+        String numTextEuro = textViewNumEuro.getText().toString();
+        int posicioUltim = 0;
+
+        if (numTextEuro != "0" && numTextEuro != null) {
+            if (numTextEuro.length() == 1) {
+                numTextEuro = "0";
+            } else {
+                numTextEuro = numTextEuro.substring(0, numTextEuro.length() - 1);
+            }
+
+        }
+
+        textViewNumEuro.setText(numTextEuro);
+    }
+
+    private void conversioMoneda(final String moneda) {
+        AlertDialog ad;
+
+        ad = new AlertDialog.Builder(this).create();
+        ad.setTitle("A " + moneda);
+        ad.setMessage("Quants EURO són 1 " + moneda + " ?");
+
+        // Ahora forzamos que aparezca el editText
+        final EditText edtValor = new EditText(this);
+        ad.setView(edtValor);
+
+        ad.setButton(AlertDialog.BUTTON_POSITIVE,"ACTUALITZAR", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                try {
+                    String valorString = edtValor.getText().toString().replaceAll(",", ".");
+                    numConvercio = Double.valueOf(valorString);
+
+                    switch (moneda) {
+                        case "LLIURE":
+                            buttonLliures.setBackgroundColor(Color.parseColor("#00EDF6"));
+                            buttonLliures.setTextColor(Color.parseColor("#030303"));
+                            break;
+                        case "DOLLAR":
+                            buttonDollar.setBackgroundColor(Color.parseColor("#00EDF6"));
+                            buttonDollar.setTextColor(Color.parseColor("#030303"));
+                            break;
+                        case "YEN":
+                            buttonYen.setBackgroundColor(Color.parseColor("#00EDF6"));
+                            buttonYen.setTextColor(Color.parseColor("#030303"));
+                            break;
+                        case "YUAN":
+                            buttonYuan.setBackgroundColor(Color.parseColor("#00EDF6"));
+                            buttonYuan.setTextColor(Color.parseColor("#030303"));
+                            break;
+                    }
+                    MonedaSeleccionada = true;
+                    Toast.makeText(getApplicationContext(), "S'ha pres el botó ACTUALITZAR, el valor escrit és " + edtValor.getText().toString(), Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "S'ha escrit un caracter que no corresponia ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        ad.setButton(AlertDialog.BUTTON_NEGATIVE, "CANCELAR", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // no fem res.
+                //MonedaSeleccionada = false;
+            }
+        });
+
+        ad.show();
+
+        // el Show es asíncrono.
+    }
+
+    private void mostrarConvercio() {
+        DecimalFormat formatDecimal = new DecimalFormat("#.##");
+
+        String textViewNumEuroConvert = textViewNumEuro.getText().toString().replaceAll(",",".");
+
+        double textViewDouble = Double.valueOf(textViewNumEuroConvert);
+
+        textViewNumDivisa.setText(formatDecimal.format(textViewDouble * numConvercio));
+
+    }
 }
